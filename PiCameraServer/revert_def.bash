d7f3ae7647687033bf2edb677725f3744ca8c7ab
zeziba <zeziba@mail.com>
2016-10-07 12:07:50 -0600

    Update revert_def.bash

--- PiCameraServer/revert_def.bash
+++ PiCameraServer/revert_def.bash
@@ -7,10 +7,6 @@ echo "Reverting back to regular wifi"
 
 sudo ifdown wlan0
 
-if [! -f /etc/network/ifplugd-adhoc]; then
-    sudo cp /etc/default/ifplugd /etc/default/ifplugd-adhoc
-fi
-
 sudo cp /etc/default/ifplugd_backup /etc/default/ifplugd
 
 sudo cp /etc/network/interfaces_backup /etc/network/interfaces
