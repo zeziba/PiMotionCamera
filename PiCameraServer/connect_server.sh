772f3fa709cc3f32d3d56331ea3a2b0662e3073e
zeziba <zeziba@mail.com>
2016-11-05 10:54:59 -0600

    Update connect_server.sh

--- PiCameraServer/connect_server.sh
+++ PiCameraServer/connect_server.sh
@@ -4,6 +4,7 @@
 # Connects the Pi to the PINAS server to store images
 
 declare dir_loc
+delcare network_name="RPINAS"
 declare default_mnt="/mnt/smbServer"
 
 
@@ -15,7 +16,7 @@ else
     echo sudo chmod 1777 ${default_mnt}
 fi
 
-echo dir_loc=$(nmblookup "*" | grep -Pom 1 '[0-9.]{7,15}')
+echo dir_loc=$(nmblookup ${network_name} | grep -Pom 1 '[0-9.]{7,15}')
 
 echo mount -t cifs -o username=root,password=pi //${dir_loc}/data ${default_mnt} || echo "Failed to connect"
 
