256e8db72fd5180591fe2da9e17d3937bf811f40
zeziba <zeziba@mail.com>
2016-11-05 11:00:10 -0600

    Create disconnect_server.sh

---
+++ PiCameraServer/disconnect_server.sh
@@ -0,0 +1,11 @@
+# Created by Charles Engen
+
+declare mnt_location='/mnt/smbServer'
+
+echo "Starting release of ${mnt_location}"
+
+sudo umount -t auto ${mnt_location}
+
+echo "Server Disconnected"
+
+read
