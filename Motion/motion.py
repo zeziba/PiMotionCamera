4089f9d7ec998e0f306c62a72109fc56d419e00a
cengen <owenengen@gmail.com>
2016-11-11 15:31:36 -0700

    Updated motion.py to use the led_pin setting

--- Motion/motion.py
+++ Motion/motion.py
@@ -10,15 +10,15 @@ import config_parser
 config = config_parser.ConfigSelectionMap(config_parser.configFile)
 
 # Comment out following line when moved to RPi
-# GPIO.VERBOSE = False
+GPIO.VERBOSE = False
 
 INPIN = eval(config["Settings"]["pir"])  # Signal in
-#OUTPIN = eval(config["Settings"]["led_pin"])  # LED
+OUTPIN = eval(config["Settings"]["led_pin"])  # LED
 
 GPIO.setwarnings(eval(config["Settings"]["warnings"]))
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(INPIN, GPIO.IN)
-# GPIO.setup(OUTPIN, GPIO.OUT)
+GPIO.setup(OUTPIN, GPIO.OUT)
 
 GPIO.add_event_detect(INPIN, GPIO.RISING)
 
